f7272fd9c4299fb8d2391345eae01f9854118dcd
I E <nighthawk6389@yahoo.com>
2012-12-03 21:08:53 -0500

    Update classifier with new features.txt

--- classifier.m
+++ classifier.m
@@ -23,6 +23,10 @@ OpenParenL	OpenParenR OpenParenOpp	CloseParenL	CloseParenR CloseParenOpp
 %}
 
 global NUM_LINES_INDEX;
+global WHILE_INDEX;
+global FOR_INDEX;
+global IF_INDEX;
+global CSS_INDEX;
 global ASSIGNMENTS_INDEX;
 global EQUALITY_INDEX;
 global EQUALITYOPP_INDEX;
@@ -32,8 +36,22 @@ global OPEN_PARENOPP_INDEX;
 global CLOSE_PARENL_INDEX;
 global CLOSE_PARENR_INDEX;
 global CLOSE_PARENOPP_INDEX;
+global IF_IFPAREN_INDEX;
+global IF_PARENCOND_INDEX;
+global IF_CONDPAREN_INDEX;
+global WHILE_WHILEPAREN_INDEX;
+global WHILE_PARENCOND_INDEX;
+global WHILE_CONDPAREN_INDEX;
+global FOR_FORPAREN_INDEX;
+global FOR_PARENVAR_INDEX;
+global FOR_SEMICOND_INDEX;
+global FOR_SEMIINC_INDEX;
 
 NUM_LINES_INDEX     = 3;
+WHILE_INDEX         = 4;
+FOR_INDEX           = 5;
+IF_INDEX            = 6;
+CSS_INDEX           = 9;
 ASSIGNMENTS_INDEX   = 10;
 EQUALITY_INDEX      = 12;
 EQUALITYOPP_INDEX   = 13;
@@ -43,8 +61,19 @@ OPEN_PARENOPP_INDEX = 16;
 CLOSE_PARENL_INDEX  = 17;
 CLOSE_PARENR_INDEX  = 18;
 CLOSE_PARENOPP_INDEX = 19;
+IF_IFPAREN_INDEX = 20;
+IF_PARENCOND_INDEX = 21;
+IF_CONDPAREN_INDEX = 22;
+WHILE_WHILEPAREN_INDEX = 23;
+WHILE_PARENCOND_INDEX = 24;
+WHILE_CONDPAREN_INDEX = 25;
+FOR_FORPAREN_INDEX = 26;
+FOR_PARENVAR_INDEX = 27;
+FOR_SEMICOND_INDEX = 28;
+FOR_SEMIINC_INDEX = 29;
 
-C = textscan(fTrainIn, '%s %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f');
+C = textscan(fTrainIn, '%s %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f');
+                                    %5             %10            %15           %20            %25             %30    
 fclose(fTrainIn);
 
 allLabels = C{1};
@@ -55,7 +84,7 @@ featCount = size(trainFeats, 2);
 
 trainOutLabels = cell(trainSampleCount, 1);
 
-featuresUsing = [1 2 4 5 6 7 10 12 14 15];
+featuresUsing = [1 2 4 5 6 7 9 10 12 14 18 20 21 23]; 
 
 %{
 FLOAT MAY BE A CLOSER APPROXIMATION?
@@ -106,26 +135,54 @@ for i = 1:FOLDS
     end
     % Normalize train data
     trainNormalized = doAllNormalization(trainRows);
-        
-    
+
     % Get distance
     counter = 0;
     for j = 1:size(testNormalized,1)
         
         min_index = -1;
+        second_min_index = -1;
+        third_min_index = -1;
         min_dist = 100000;
+        second_min_dist = 100000;
+        third_min_dist = 100000;
         
         for k = 1: size(trainNormalized,1)
             distance = dist( testNormalized(j,:), trainNormalized(k,:), featuresUsing);
-            % Compare newest distance to the current max dist
-            if(distance < min_dist)
+            % Compare newest distance to the current max dist 
+            
+            if( distance < min_dist )
+                third_min_dist = second_min_dist;
+                third_min_index = second_min_index;
+                second_min_dist = min_dist;
+                second_min_index = min_index;
                 min_dist = distance;
                 min_index = k;
+            elseif( distance < second_min_dist )
+                third_min_dist = second_min_dist;
+                third_min_index = second_min_index;
+                second_min_dist = distance;
+                second_min_index = k;
+            elseif( distance < third_min_dist )
+                third_min_dist = distance;
+                third_min_index = k;
             end
+            
+
+            
         end
         
         %fprintf('Checking %s(%d). Classified as %s(%d). Min Dist: %d\n',...
          %       testLabels{j},j,trainLabels{min_index},min_index,min_dist);
+         
+         if( strcmp(trainLabels{min_index},trainLabels{second_min_index}) == 0 ||...
+             strcmp(trainLabels{min_index},trainLabels{third_min_index}) == 0)
+            fprintf('NOT EQUALS: Actual - %s   %s(%f) %s(%f) %s(%f)\n',...
+                    testLabels{j}, trainLabels{min_index}, min_dist,...
+                    trainLabels{second_min_index}, second_min_dist,...  
+                    trainLabels{third_min_index}, third_min_dist);
+         end
+         
         if( strcmp(testLabels{j},trainLabels{min_index}) == 1 )
             counter = counter + 1;
         end
@@ -142,7 +199,10 @@ end
 
 function normFeatures = doAllNormalization(features)
 
-    global ASSIGNMENTS_INDEX;
+    global WHILE_INDEX;
+    global FOR_INDEX;
+    global IF_INDEX;
+    global CSS_INDEX;
     global EQUALITY_INDEX;
     global EQUALITYOPP_INDEX;
     global OPEN_PARENL_INDEX;
@@ -151,13 +211,30 @@ function normFeatures = doAllNormalization(features)
     global CLOSE_PARENL_INDEX;
     global CLOSE_PARENR_INDEX;
     global CLOSE_PARENOPP_INDEX;
+    global IF_IFPAREN_INDEX;
+    global IF_PARENCOND_INDEX;
+    global IF_CONDPAREN_INDEX;
+    global WHILE_WHILEPAREN_INDEX;
+    global WHILE_PARENCOND_INDEX;
+    global WHILE_CONDPAREN_INDEX;
+    global FOR_FORPAREN_INDEX;
+    global FOR_PARENVAR_INDEX;
+    global FOR_SEMICOND_INDEX;
+    global FOR_SEMIINC_INDEX;
 
-    normFeatures = normalizeByLines(features,1,ASSIGNMENTS_INDEX - 1);
-    normFeatures = normalizeByOpp(normFeatures,[EQUALITY_INDEX],EQUALITYOPP_INDEX);
+    normFeatures = normalizeByLines(features,1,CSS_INDEX - 1);
+    normFeatures = normalizeByOpp(normFeatures,[CSS_INDEX],[WHILE_INDEX FOR_INDEX IF_INDEX]);
+    normFeatures = normalizeByOpp(normFeatures,[EQUALITY_INDEX],[EQUALITYOPP_INDEX]);
     normFeatures = normalizeByOpp(normFeatures,...
-                    [OPEN_PARENL_INDEX OPEN_PARENR_INDEX],OPEN_PARENOPP_INDEX);
+                    [OPEN_PARENL_INDEX OPEN_PARENR_INDEX],[OPEN_PARENOPP_INDEX]);
     normFeatures = normalizeByOpp(normFeatures,...
-                    [CLOSE_PARENL_INDEX CLOSE_PARENR_INDEX],CLOSE_PARENOPP_INDEX);
+                    [CLOSE_PARENL_INDEX CLOSE_PARENR_INDEX],[CLOSE_PARENOPP_INDEX]);
+    normFeatures = normalizeByOpp(normFeatures,...
+                    [IF_IFPAREN_INDEX IF_PARENCOND_INDEX IF_CONDPAREN_INDEX],[IF_INDEX]);
+    normFeatures = normalizeByOpp(normFeatures,...
+                    [WHILE_WHILEPAREN_INDEX WHILE_PARENCOND_INDEX WHILE_CONDPAREN_INDEX],[WHILE_INDEX]);
+    normFeatures = normalizeByOpp(normFeatures,...
+                    [FOR_FORPAREN_INDEX FOR_PARENVAR_INDEX FOR_SEMICOND_INDEX FOR_SEMIINC_INDEX],[FOR_INDEX]);            
                 
     normFeatures = normalizeData(normFeatures);
 
@@ -186,15 +263,19 @@ end
 function normOpp = normalizeByOpp(features,indices,oppIndex)
 
     normOpp = features;
+    oppValue = 0;
     
     for i = 1:size(features,1)
         for j = 1:size(indices,2)
             index = indices(j);
-            oppValue = features(i,oppIndex);
+            oppValue = 0;
+            for k = 1: size(oppIndex,2)
+                oppValue = oppValue + features(i,oppIndex(k)); 
+            end
             if( oppValue == 0 )
                 continue;
             end
-            %fprintf('Index: %d, OppIndex: %d, OppValue: %f\n',index,oppIndex,oppValue);
+            %fprintf('Index: %d, OppIndex: %d, OppValue: %f\n',index,oppIndex(1),oppValue);
             normOpp(i,index) = features(i,index)/oppValue;
         end
     end
